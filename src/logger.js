89d6cd37a8a9b398d9dd115a572a2ca71ee950d6
Pat Cavit <github@patcavit.com>
2025-02-10 12:16:26 -0800

    refactor: pass around state some
    Lets us get away from redefining these functions every new machine, which is nice I guess

--- src/logger.js
+++ src/logger.js
@@ -2,61 +2,66 @@ const machines = new Map();
 
 const ROOT_ID = "root";
 
-const stateTracker = (machine, logger) => {
-    const walker = (machine, id = ROOT_ID) => {
-        let lastState = machine.getSnapshot();
+const findChildren = (id, state, logger) => {
+	if(!state.children) {
+		return;
+	}
 
-        logger(id, lastState.value);
+	for(const name in state.children) {
+		const child = state.children[name];
 
-        const { unsubscribe } = machine.subscribe({
-            next(state) {
-                if(state === lastState) {
-                    return;
-                }
+		// Not a statechart, ignore it
+		if(!child?.logic?.__xstatenode) {
+			continue;
+		}
 
-                lastState = state;
+		const path = id === ROOT_ID ? name : `${id}.${name}`;
 
-                // Return all updated state values
-                logger(id, state.value);
-
-                if(!state.children) {
-                    return;
-                }
-                
-                for(const name in state.children) {
-                    const child = state.children[name];
-
-                    // Not a statechart, ignore it
-                    if(!child?.logic?.__xstatenode) {
-                        continue;
-                    }
-                
-                    const path = id !== ROOT_ID ? `${id}.${name}` : name;
-                    
-                    if(!machines.has(path)) {
-                        walker(child, path);
-                    }
-                }
-            },
-
-            // Clean up when finished
-            complete() {
-                if(!machines.has(id)) {
-                    return;
-                }
-
-                machines.get(id)();
-                machines.delete(id);
-                lastState = null;
-            }
-        });
-
-        machines.set(id, unsubscribe);
-
-        return unsubscribe;
-    };
-
-    return walker(machine);
+		if(!machines.has(path)) {
+			// eslint-disable-next-line no-use-before-define -- It's fine, but weird I agree
+			walker(path, child, logger);
+		}
+	}
 };
+
+const handleState = (id, state, logger) => {
+	logger(id, state.value, state);
+	findChildren(id, state, logger);
+};
+
+const walker = (id, machine, logger) => {
+	let lastState = machine.getSnapshot();
+
+	handleState(id, lastState, logger);
+
+	const { unsubscribe } = machine.subscribe({
+		next(state) {
+			if(state === lastState) {
+				return;
+			}
+
+			lastState = state;
+
+			handleState(id, state, logger);
+		},
+
+		// Clean up when finished
+		complete() {
+			if(!machines.has(id)) {
+				return;
+			}
+
+			machines.get(id)();
+			machines.delete(id);
+			lastState = undefined;
+		},
+	});
+
+	machines.set(id, unsubscribe);
+
+	return unsubscribe;
+};
+
+const stateTracker = (machine, logger) => walker(ROOT_ID, machine, logger);
 
 export default stateTracker;
